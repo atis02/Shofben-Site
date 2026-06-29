e9dfff203827b412ed1aff6c0f194fef871b95bf
atis02 <guwancikramow@gmail.com>
2026-06-29 18:08:43 +0300

    fix analytics id

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -4,6 +4,6 @@ export const SITE_URL =
 export const SITE_NAME = "Hermetik Şofben Servisi";
 
 export const GA_MEASUREMENT_ID =
-  process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID ?? "G-HZR3934FB5";
+  process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID ?? "AW-18277896842";
 
 export const DEFAULT_OG_IMAGE = `${SITE_URL}/logo.avif`;
